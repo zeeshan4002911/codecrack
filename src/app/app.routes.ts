c25baabee3c07fa116e578b8e78bfb3cf826a31c
zeesh <zeeshan4002911@gmail.com>
2025-11-20 09:57:09 +0530

    Moved app to layout new component, to use router-outler for query params support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
-// import { EditorView } from './editor-view/editor-view';
-// import { DiffCheckerView } from './diff-checker-view/diff-checker-view';
 
 export const routes: Routes = [
-    { path: "", component: App },
-    // { path: "editor", component: EditorView },
-    // { path: "diff-checker", component: DiffCheckerView },
+    {
+        path: ":codeShareId",
+        loadComponent: () => import('./layout/layout').then(m => m.Layout)
+    },
+    {
+        path: "",
+        loadComponent: () => import('./layout/layout').then(m => m.Layout)
+    },
     { path: "**", redirectTo: "", pathMatch: "full" }
 ];
